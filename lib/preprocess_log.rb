f83adb8105a3d572b52d93b19f804bc3cc7ed775
Chris Skeens <cskee004@odu.edu>
2025-04-04 14:00:20 -0400

    Implemented various parsers to extract data from auth.log

--- lib/preprocess_log.rb
+++ lib/preprocess_log.rb
@@ -1,79 +1,257 @@
-# Example of a suspicous message found in auth.log
-# 15:59:42 ip-10-77-20-248 sshd[3242]: error: maximum authentication attempts exceeded for root from 186.128.152.44 port 34605 ssh2 [preauth]
-# ip-10-77-20-248 = Hostname
-# sshd[3242] = sshd is logging the event for PID 3242
-# error = log level
-# for root = target user account
-# from 186.128.152.44 = source ip
-# port 34605 = source port on attackers machine
-# ssh2 = SSH protocol used
-# [preauth] = log entry occurred before authentication
+require 'pry-byebug'
 
-test_string = "Mar 28 07:36:32 ip-10-77-20-248 sshd[22419]: error: maximum authentication attempts exceeded for invalid user admin from 14.185.87.49 port 47825 ssh2 [preauth]"
-
-log_levels = 
-[
-  "emerg",
-  "alert",
-  "crit",
-  "error",
-  "warn",
-  "notice",
-  "info",
-  "debug"
-]
-meta_data = 
-[
-  :Line_number,
-  :Date,
-  :Time,
-  :Host,
-  :PID,
-  :Message,
-  :User,
-  :Source_IP,
-  :Source_port
-]
 
 # This method reads from the given file path. If a file is not given, then read_log uses default file located in the data directory.
-# If line contains any of the log level keywords, then call parse_string with the single line and line number
+# If a single line contains any of the log level keywords, then call parse_string with the single line and line number
 def read_log(filename = "./data/auth.log")
-  File.foreach(filename).with_index do |line, line_num| if line.include?('error') then parse_error_line(line, line_num)
+  File.foreach(filename).with_index do |line, line_num|
+    case line
+    when /error/
+      parse_error(line, line_num)
+    when /authentication failure/
+      parse_auth_failure(line, line_num)
+    when /Disconnected/
+      parse_disconnect(line, line_num)
+    when /session opened/
+      parse_session_open(line, line_num)
+    when /session closed/
+      parse_session_close(line, line_num)
+    when /(PWD)*(USER)*(COMMAND)/
+      parse_sudo_command(line, line_num)
+    when /Accepted/
+      parse_accept_event(line, line_num)
+    end
   end
 end
-end
 
 
 
-def parse_error_line(line, line_num)
+def parse_error(line, line_num)
   
   date = line.match(/^\D{4}\d{2}/)
   time = line.match(/\d{2}\:\d{2}\:\d{2}/)
   host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
   pid = line.match(/\[(\d+)\]/)
-  pid_num = pid[1] if pid
   message = line.match(/(error)(.*?)(?=for)/)
   user = line.match(/(\w+)(?=\s+from)/)
   src_ip = line.match(/\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}/)
-  puts src_ip
   port = line.match(/port\s(\d{5})/)
-  src_port = port[1] if port
-
-  log_pattern = /(^\D{4}\d{2}) (\d{2}\:\d{2}\:\d{2}) (\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}) (\[(\d+)\]) ((error)(.*?)(?=for)) ((\w+)(?=\s+from)) (\d{2}\.\d{3}\.\d{3}\.\d{3}) (port\s(\d{5}))/
   
-  line_hash = 
+  pid_num = pid[1] if pid
+  src_port = port[1] if port
+  m = message[0].rstrip() if message
+  
+  # log_pattern = /(^\D{4}\d{2}) (\d{2}\:\d{2}\:\d{2}) (\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}) (\[(\d+)\]) ((error)(.*?)(?=for)) ((\w+)(?=\s+from)) (\d{2}\.\d{3}\.\d{3}\.\d{3}) (port\s(\d{5}))/
+  
+  error_hash = 
   {
     Line_number: line_num, 
     Date: date[0], 
     Time: time[0], 
     Host: host[0], 
     PID: pid_num, 
-    Message: message[0], 
+    Message: m, 
     User: user[0], 
     Source_IP: src_ip[0], 
     Source_port: src_port 
   }
   
+end
+
+def parse_auth_failure(line, line_num)
+  
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  pid = line.match(/\[(\d+)\]/)
+  message = line.match(/(Disconnecting)(.*?)(?<=failures)/)
+  auth_stage = line.match(/\[(\D+)\]/)
+
+  pid_num = pid[1] if pid
+  m = message[0].rstrip() if message
+
+  auth_hash = 
+  {
+    Line_number: line_num, 
+    Date: date[0], 
+    Time: time[0], 
+    Host: host[0], 
+    PID: pid_num, 
+    Message: m, 
+    Stage: auth_stage[1]
+  }
+end
+
+def parse_disconnect(line, line_num)
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  pid = line.match(/\[(\d+)\]/)
+  message = line.match(/(Disconnected)(.*?)(?=from)/)
+  user = line.match(/(\w+)(?=\s+from)/)
+  src_ip = line.match(/\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}/)
+  port = line.match(/port\s(\d{5})/)
+  
+  pid_num = pid[1] if pid
+  src_port = port[1] if port
+  m = message[0].rstrip() if message
+
+  disconnect_hash = 
+  {
+    Line_number: line_num, 
+    Date: date[0], 
+    Time: time[0], 
+    Host: host[0], 
+    PID: pid_num, 
+    Message: m,  
+    Source_IP: src_ip[0], 
+    Source_port: src_port 
+  }
+end
+
+def parse_session_open(line, line_num) #11
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  message = line.match(/(sudo:)(.*?)(?=for)/)
+  user = line.match(/(?<=user\s)(\w+)/)
+
+  m = message[0].rstrip() if message
+  u = user[0] if user
+
+  session_open_hash =
+  {
+    Line_number: line_num, 
+    Date: date[0], 
+    Time: time[0], 
+    Host: host[0],  
+    Message: m, 
+    User: u
+  }
+end
+
+def parse_session_close(line, line_num)
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  message = line.match(/(sudo:)(.*?)(?=for)/)
+  user = line.match(/(?<=user\s)(\w+)/)
+
+  m = message[0].rstrip() if message
+  u = user[0] if user
+
+  session_open_hash =
+  {
+    Line_number: line_num, 
+    Date: date[0], 
+    Time: time[0], 
+    Host: host[0],  
+    Message: m, 
+    User: u
+  }
+end
+
+def parse_sudo_command(line, line_num)
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  pwd = line.match(/(?<=PWD=)(.*?)(?=\s+\;)/)
+  user = line.match(/(?<=USER=)(.*?)(?=\s+\;)/)
+  command = line.match(/(?<=COMMAND=)(.*)/)
+
+  sudo_command_hash = 
+  {
+    Line_number: line_num,
+    Date: date[0],
+    Time: time[0], 
+    Host: host[0],
+    Directory: pwd[0],
+    User: user[0],
+    Command: command[0]
+  }
+end
+
+def parse_accept_event(line, line_num)
+  
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  pid = line.match(/\[(\d+)\]/)
+  message = line.match(/(Accepted)(.*?)(?=\s+for)/)
+  user = line.match(/(\w+)(?=\s+from)/)
+  src_ip = line.match(/\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}/)
+  port = line.match(/port\s(\d{5})/)
+  key = line.match(/(?<=SHA256:)(\w+)/)
+  
+  pid_num = pid[1] if pid
+  src_port = port[1] if port
+  m = message[0].rstrip() if message
+  
+  error_hash = 
+  {
+    Line_number: line_num, 
+    Date: date[0], 
+    Time: time[0], 
+    Host: host[0], 
+    PID: pid_num, 
+    Message: m, 
+    User: user[0], 
+    Source_IP: src_ip[0], 
+    Source_port: src_port,
+    Key: key[0]
+  }
+end
+
+def parse_invalid_user(line, line_num)
+  
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  pid = line.match(/\[(\d+)\]/)
+  message = "Invalid user"
+  user = line.match(/(\w+)(?=\s+from)/)
+  src_ip = line.match(/\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}/)
+  
+  pid_num = pid[1] if pid
+  
+  invalid_hash = 
+  {
+    Line_number: line_num, 
+    Date: date[0], 
+    Time: time[0], 
+    Host: host[0], 
+    PID: pid_num, 
+    Message: message, 
+    User: user[0], 
+    Source_IP: src_ip[0],
+  }
+end
+
+def parse_failed_password(line, line_num)
+  date = line.match(/^\D{4}\d{2}/)
+  time = line.match(/\d{2}\:\d{2}\:\d{2}/)
+  host = line.match(/\D{3}\d{2}\-\d{2}\-\d{2}-\d{3}/)
+  pid = line.match(/\[(\d+)\]/)
+  message = "Failed password"
+  user = line.match(/(\w+)(?=\s+from)/)
+  src_ip = line.match(/\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}/)
+  port = line.match(/port\s(\d{5})/)
+
+  pid_num = pid[1] if pid
+  src_port = port[1] if port
+
+  failed_hash = 
+  {
+    Line_number: line_num, 
+    Date: date[0], 
+    Time: time[0], 
+    Host: host[0], 
+    PID: pid_num, 
+    Message: message, 
+    User: user[0], 
+    Source_IP: src_ip[0],
+    Source_port: src_port
+  }
 end
 
 read_log("./data/auth-test.log")
--- spec/preprocess_log_spec.rb
+++ spec/preprocess_log_spec.rb
@@ -1,14 +1,14 @@
 require "spec_helper"
 require "preprocess_log"
 
-example_1 = "Mar 31 10:36:28 ip-10-77-20-248 sshd[19551]: error: maximum authentication attempts exceeded for root from 122.191.89.89 port 37753 ssh2 [preauth]" #3903
-example_2 = "Mar 31 11:06:50 ip-10-77-20-248 sshd[19710]: error: maximum authentication attempts exceeded for invalid user ajay from 42.184.142.151 port 47882 ssh2 [preauth]" #3932
+
 
 describe "Preprocess log" do
-  describe "parse example 1" do
-    
-    it "returns hash containing meta data from example 1 " do
-      result = parse_error_line(example_1, 3903)
+
+  error = "Mar 31 10:36:28 ip-10-77-20-248 sshd[19551]: error: maximum authentication attempts exceeded for root from 122.191.89.89 port 37753 ssh2 [preauth]" #3903
+  describe "parse error flag" do
+    it "returns hash meta data for error flags" do
+      result = parse_error(error, 3903)
       expected = 
       {
         Line_number: 3903, 
@@ -23,23 +23,156 @@ describe "Preprocess log" do
       }
       expect(result).to eq(expected)
     end
-
-    xit "returns hash containing meta data from example 2 " do
-      result = parse_error_line(example_2, 3932)
+  end
+  
+  auth_fail = "Mar 27 14:01:39 ip-10-77-20-248 sshd[2938]: Disconnecting: Too many authentication failures [preauth]" #87
+  describe "parse authentication failure" do
+    it "returns hash meta data for authenitcation failure" do
+      result = parse_auth_failure(auth_fail, 87)
       expected = 
       {
-        Line_number: 3932, 
-        Date: "Mar 31", 
-        Time: "11:06:50", 
-        Host: "ip-10-77-20-248", 
-        PID: "19710", 
-        Message: "error: maximum authentication attempts exceeded", 
-        User: "ajay",  
-        Source_IP: "42.184.142.151", 
-        Source_port: "47882", 
+        Line_number: 87,
+        Date: "Mar 27",
+        Time: "14:01:39",
+        Host: "ip-10-77-20-248",
+        PID: "2938",
+        Message: "Disconnecting: Too many authentication failures",
+        Stage: "preauth"
       }
       expect(result).to eq(expected)
     end
-    
+  end
+
+  disconnect = "Mar 27 14:02:16 ip-10-77-20-248 sshd[2856]: Disconnected from 85.245.107.41 port 54866" #89
+  describe "parse disconnected event" do
+    it "returns hash meta data for a disonnected event" do
+      result = parse_disconnect(disconnect, 89)
+      expected = 
+      {
+        Line_number: 89, 
+        Date: "Mar 27", 
+        Time: "14:02:16", 
+        Host: "ip-10-77-20-248", 
+        PID: "2856", 
+        Message: "Disconnected",  
+        Source_IP: "85.245.107.41", 
+        Source_port: "54866", 
+      }
+      expect(result).to eq(expected)
+    end
+  end
+
+  open = "Mar 27 13:09:37 ip-10-77-20-248 sudo: pam_unix(sudo:session): session opened for user root by ubuntu(uid=0)" #11
+  describe "parse session opened event" do
+    it "returns hash meta data for session open event" do
+      result = parse_session_open(open, 11)
+      expected = 
+      {
+        Line_number: 11, 
+        Date: "Mar 27", 
+        Time: "13:09:37", 
+        Host: "ip-10-77-20-248", 
+        Message: "sudo: pam_unix(sudo:session): session opened",
+        User: "root",  
+      }
+      expect(result).to eq(expected)
+    end
+  end
+
+  close = "Mar 27 13:09:38 ip-10-77-20-248 sudo: pam_unix(sudo:session): session closed for user root" #12
+  describe "parse session closed event" do
+    it "returns hash meta data for session closed event" do
+      result = parse_session_close(close, 12)
+      expected = 
+      {
+        Line_number: 12, 
+        Date: "Mar 27", 
+        Time: "13:09:38", 
+        Host: "ip-10-77-20-248", 
+        Message: "sudo: pam_unix(sudo:session): session closed",
+        User: "root",  
+      }
+      expect(result).to eq(expected)
+    end
+  end
+  
+  sudo = "Mar 27 13:11:35 ip-10-77-20-248 sudo:   ubuntu : TTY=pts/0 ; PWD=/home/ubuntu ; USER=root ; COMMAND=/usr/bin/apt-get install packetbeat" #37
+  describe "parse sudo command event" do
+    it "returns hash meta data for sudo command event" do
+      result = parse_sudo_command(sudo, 37)
+      expected = 
+      {
+        Line_number: 37, 
+        Date: "Mar 27", 
+        Time: "13:11:35", 
+        Host: "ip-10-77-20-248", 
+        Directory: "/home/ubuntu",
+        User: "root",
+        Command: "/usr/bin/apt-get install packetbeat"  
+      }
+      expect(result).to eq(expected)
+    end
+  end
+
+  accepted = "Mar 28 14:09:55 ip-10-77-20-248 sshd[29069]: Accepted publickey for ubuntu from 85.245.107.41 port 55779 ssh2: RSA SHA256:Kl8kPGZrTiz7g4FO1hyqHdsSBBb5Fge6NWOobN03XJg" #841
+  describe "parse accepted event" do
+    it "returns hash meta data for accept event" do
+      result = parse_accept_event(accepted, 841)
+      expected = 
+      {
+        Line_number: 841, 
+        Date: "Mar 28", 
+        Time: "14:09:55", 
+        Host: "ip-10-77-20-248", 
+        PID: "29069", 
+        Message: "Accepted publickey",  
+        User: "ubuntu", 
+        Source_IP: "85.245.107.41", 
+        Source_port: "55779", 
+        Key: "Kl8kPGZrTiz7g4FO1hyqHdsSBBb5Fge6NWOobN03XJg"
+      }
+      expect(result).to eq(expected)
+    end
+  end
+  
+  invalid_user = "Mar 31 06:34:36 ip-10-77-20-248 sshd[18539]: Invalid user pruebas from 60.187.118.40" #3741
+  describe "parse invalid user event" do
+    it "returns hash meta data for invalid user event" do
+      result = parse_invalid_user(invalid_user, 3741)
+      expected = 
+      {
+        Line_number: 3741, 
+        Date: "Mar 31", 
+        Time: "06:34:36", 
+        Host: "ip-10-77-20-248", 
+        PID: "18539", 
+        Message: "Invalid user",  
+        User: "pruebas", 
+        Source_IP: "60.187.118.40", 
+      }
+      expect(result).to eq(expected)
+    end
+  end
+  
+  failed_password = "Mar 31 06:34:38 ip-10-77-20-248 sshd[18539]: Failed password for invalid user pruebas from 60.187.118.40 port 41838 ssh2" #3745
+  describe "parse failed password event" do
+    it "returns hash meta data for failed password event" do
+      result = parse_failed_password(failed_password, 3745)
+      expected = 
+      {
+        Line_number: 3745, 
+        Date: "Mar 31", 
+        Time: "06:34:38", 
+        Host: "ip-10-77-20-248", 
+        PID: "18539", 
+        Message: "Failed password",  
+        User: "pruebas", 
+        Source_IP: "60.187.118.40", 
+        Source_port: "41838"
+      }
+      expect(result).to eq(expected)
+    end
   end
 end
+
+
