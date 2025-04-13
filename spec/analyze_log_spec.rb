129df02999b2e2e55eb52cb673de074bef4089a8
Chris Skeens <cskee004@odu.edu>
2025-04-13 06:25:55 -0400

    rubocop

--- spec/analyze_log_spec.rb
+++ spec/analyze_log_spec.rb
@@ -1,38 +1,38 @@
-require "spec_helper"
-require "preprocess_log"
-require "analyze_log.rb"
+require 'spec_helper'
+require 'preprocess_log'
+require 'analyze_log'
 
-describe "Analyze Log" do
+describe 'Analyze Log' do
   let(:log_parser) { LogParser.new }
   let(:log_analyzer) { LogAnalyzer.new(log_parser) }
-  let(:log) { log_parser.read_log() }
+  let(:log) { log_parser.read_log }
 
-  describe "suspicious_ips" do
-    it "finds each high security event for each IP" do
+  describe 'suspicious_ips' do
+    it 'finds each high security event for each IP' do
       result = log_analyzer.suspicious_ips(log)
-      expect(result["178.219.248.139"]).to eq(8)
+      expect(result['178.219.248.139']).to eq(8)
     end
   end
 
-  describe "events_by_hour" do
-    it "counts how many times each event occurs by the hour" do
+  describe 'events_by_hour' do
+    it 'counts how many times each event occurs by the hour' do
       result = log_analyzer.events_by_hour(log)
-      expect(result[:Auth_failure]["10"]).to eq(10)
+      expect(result[:Auth_failure]['10']).to eq(10)
     end
   end
 
-  describe "events_by_date" do
-    it "counts how many times each event occurs by the date" do
+  describe 'events_by_date' do
+    it 'counts how many times each event occurs by the date' do
       result = log_analyzer.events_by_date(log)
-      expect(result[:Sudo_command]["2025-04-03"]).to eq(21)
+      expect(result[:Sudo_command]['2025-04-03']).to eq(21)
     end
   end
 
-  describe "login_patterns" do
-    it "finds when accepted password and failed password events occur" do
+  describe 'login_patterns' do
+    it 'finds when accepted password and failed password events occur' do
       result = log_analyzer.login_patterns(log)
-      expect(result[:Accepted_password]["20"]).to eq(7)
-      expect(result[:Failed_password]["00"]).to eq(12)
+      expect(result[:Accepted_password]['20']).to eq(7)
+      expect(result[:Failed_password]['00']).to eq(12)
     end
   end
 end
--- spec/preprocess_log_spec.rb
+++ spec/preprocess_log_spec.rb
@@ -1,198 +1,202 @@
 require 'spec_helper'
 require 'preprocess_log'
 
-describe "Preprocess log" do
+describe 'Preprocess log' do
   log_parser = LogParser.new
 
-  error = "Mar 31 10:36:28 ip-10-77-20-248 sshd[19551]: error: maximum authentication attempts exceeded for root from 122.191.89.89 port 37753 ssh2 [preauth]" # 3903
-  describe "parse_error" do
+  error = 'Mar 31 10:36:28 ip-10-77-20-248 sshd[19551]: error: maximum authentication attempts exceeded for root from
+  122.191.89.89 port 37753 ssh2 [preauth]' # 3903
+  describe 'parse_error' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for error event" do
-      result = log_parser.parse_error(error, 3903, "Error flag", "Mar 31", "10:36:28", "ip-10-77-20-248")
+    it 'returns a hash of meta data for error event' do
+      result = log_parser.parse_error(error, 3903, 'Error flag', 'Mar 31', '10:36:28', 'ip-10-77-20-248')
       expected =
         {
           Line_number: 3903,
-          Type: "Error flag",
-          Date: "Mar 31",
-          Time: "10:36:28",
-          Host: "ip-10-77-20-248",
-          PID: "19551",
-          Message: "error: maximum authentication attempts exceeded",
-          User: "root",
-          Source_IP: "122.191.89.89",
-          Source_port: "37753",
+          Type: 'Error flag',
+          Date: 'Mar 31',
+          Time: '10:36:28',
+          Host: 'ip-10-77-20-248',
+          PID: '19551',
+          Message: 'error: maximum authentication attempts exceeded',
+          User: 'root',
+          Source_IP: '122.191.89.89',
+          Source_port: '37753'
         }
       expect(result).to eq(expected)
     end
   end
 
-  auth_fail = "Mar 27 14:01:39 ip-10-77-20-248 sshd[2938]: Disconnecting: Too many authentication failures [preauth]" # 87
-  describe "parse_auth_failure" do
+  auth_fail = 'Mar 27 14:01:39 ip-10-77-20-248 sshd[2938]: Disconnecting: Too many authentication failures [preauth]'
+  describe 'parse_auth_failure' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for a authentication failure event" do
-      result = log_parser.parse_auth_failure(auth_fail, 87, "Authentication failure", "Mar 27", "14:01:39",
-                                             "ip-10-77-20-248")
+    it 'returns a hash of meta data for a authentication failure event' do
+      result = log_parser.parse_auth_failure(auth_fail, 87, 'Authentication failure', 'Mar 27', '14:01:39',
+                                             'ip-10-77-20-248')
       expected =
         {
           Line_number: 87,
-          Type: "Authentication failure",
-          Date: "Mar 27",
-          Time: "14:01:39",
-          Host: "ip-10-77-20-248",
-          PID: "2938",
-          Message: "Disconnecting: Too many authentication failures",
+          Type: 'Authentication failure',
+          Date: 'Mar 27',
+          Time: '14:01:39',
+          Host: 'ip-10-77-20-248',
+          PID: '2938',
+          Message: 'Disconnecting: Too many authentication failures'
         }
       expect(result).to eq(expected)
     end
   end
 
-  disconnect = "Mar 27 14:02:16 ip-10-77-20-248 sshd[2856]: Disconnected from 85.245.107.41 port 54866" # 89
-  describe "parse_disconnected" do
+  disconnect = 'Mar 27 14:02:16 ip-10-77-20-248 sshd[2856]: Disconnected from 85.245.107.41 port 54866'
+  describe 'parse_disconnected' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for a disconnected event" do
+    it 'returns a hash of meta data for a disconnected event' do
       log_parser = LogParser.new
-      result = log_parser.parse_disconnect(disconnect, 89, "Disconnect", "Mar 27", "14:02:16", "ip-10-77-20-248")
+      result = log_parser.parse_disconnect(disconnect, 89, 'Disconnect', 'Mar 27', '14:02:16', 'ip-10-77-20-248')
       expected =
         {
           Line_number: 89,
-          Type: "Disconnect",
-          Date: "Mar 27",
-          Time: "14:02:16",
-          Host: "ip-10-77-20-248",
-          PID: "2856",
-          Source_IP: "85.245.107.41",
-          Source_port: "54866",
+          Type: 'Disconnect',
+          Date: 'Mar 27',
+          Time: '14:02:16',
+          Host: 'ip-10-77-20-248',
+          PID: '2856',
+          Source_IP: '85.245.107.41',
+          Source_port: '54866'
         }
       expect(result).to eq(expected)
     end
   end
 
-  open = "Mar 27 13:09:37 ip-10-77-20-248 sudo: pam_unix(sudo:session): session opened for user root by ubuntu(uid=0)" # 11
-  describe "parse_session_open" do
+  open = 'Mar 27 13:09:37 ip-10-77-20-248 sudo: pam_unix(sudo:session): session opened for user root by ubuntu(uid=0)'
+  describe 'parse_session_open' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for a session open event" do
-      result = log_parser.parse_session_open(open, 11, "Session opened", "Mar 27", "13:09:37", "ip-10-77-20-248")
+    it 'returns a hash of meta data for a session open event' do
+      result = log_parser.parse_session_open(open, 11, 'Session opened', 'Mar 27', '13:09:37', 'ip-10-77-20-248')
       expected =
         {
           Line_number: 11,
-          Type: "Session opened",
-          Date: "Mar 27",
-          Time: "13:09:37",
-          Host: "ip-10-77-20-248",
-          User: "root",
+          Type: 'Session opened',
+          Date: 'Mar 27',
+          Time: '13:09:37',
+          Host: 'ip-10-77-20-248',
+          User: 'root'
         }
       expect(result).to eq(expected)
     end
   end
 
-  close = "Mar 27 13:09:38 ip-10-77-20-248 sudo: pam_unix(sudo:session): session closed for user root" # 12
-  describe "parse_session_close" do
+  close = 'Mar 27 13:09:38 ip-10-77-20-248 sudo: pam_unix(sudo:session): session closed for user root'
+  describe 'parse_session_close' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for a session closed event" do
-      result = log_parser.parse_session_close(close, 12, "Session closed", "Mar 27", "13:09:38", "ip-10-77-20-248")
+    it 'returns a hash of meta data for a session closed event' do
+      result = log_parser.parse_session_close(close, 12, 'Session closed', 'Mar 27', '13:09:38', 'ip-10-77-20-248')
       expected =
         {
           Line_number: 12,
-          Type: "Session closed",
-          Date: "Mar 27",
-          Time: "13:09:38",
-          Host: "ip-10-77-20-248",
-          User: "root",
+          Type: 'Session closed',
+          Date: 'Mar 27',
+          Time: '13:09:38',
+          Host: 'ip-10-77-20-248',
+          User: 'root'
         }
       expect(result).to eq(expected)
     end
   end
 
-  sudo = "Mar 27 13:11:35 ip-10-77-20-248 sudo:   ubuntu : TTY=pts/0 ; PWD=/home/ubuntu ; USER=root ; COMMAND=/usr/bin/apt-get install packetbeat" # 37
-  describe "parse_sudo_command" do
+  sudo = 'Mar 27 13:11:35 ip-10-77-20-248 sudo:   ubuntu : TTY=pts/0 ; PWD=/home/ubuntu ; USER=root ;
+  COMMAND=/usr/bin/apt-get install packetbeat' # 37
+  describe 'parse_sudo_command' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for a sudo command event" do
-      result = log_parser.parse_sudo_command(sudo, 37, "Sudo command", "Mar 27", "13:11:35", "ip-10-77-20-248")
+    it 'returns a hash of meta data for a sudo command event' do
+      result = log_parser.parse_sudo_command(sudo, 37, 'Sudo command', 'Mar 27', '13:11:35', 'ip-10-77-20-248')
       expected =
         {
           Line_number: 37,
-          Type: "Sudo command",
-          Date: "Mar 27",
-          Time: "13:11:35",
-          Host: "ip-10-77-20-248",
-          Directory: "/home/ubuntu",
-          User: "root",
-          Command: "/usr/bin/apt-get install packetbeat"
+          Type: 'Sudo command',
+          Date: 'Mar 27',
+          Time: '13:11:35',
+          Host: 'ip-10-77-20-248',
+          Directory: '/home/ubuntu',
+          User: 'root',
+          Command: '/usr/bin/apt-get install packetbeat'
         }
       expect(result).to eq(expected)
     end
   end
 
-  accepted = "Mar 28 14:09:55 ip-10-77-20-248 sshd[29069]: Accepted publickey for ubuntu from 85.245.107.41 port 55779 ssh2: RSA SHA256:Kl8kPGZrTiz7g4FO1hyqHdsSBBb5Fge6NWOobN03XJg" # 841
-  describe "parse_accept_event" do
+  accepted = 'Mar 28 14:09:55 ip-10-77-20-248 sshd[29069]: Accepted publickey for ubuntu from 85.245.107.41 port 55779
+  ssh2: RSA SHA256:Kl8kPGZrTiz7g4FO1hyqHdsSBBb5Fge6NWOobN03XJg' # 841
+  describe 'parse_accept_event' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for an accept event" do
-      result = log_parser.parse_accept_event(accepted, 841, "Accept event", "Mar 28", "14:09:55", "ip-10-77-20-248")
+    it 'returns a hash of meta data for an accept event' do
+      result = log_parser.parse_accept_event(accepted, 841, 'Accept event', 'Mar 28', '14:09:55', 'ip-10-77-20-248')
       expected =
         {
           Line_number: 841,
-          Type: "Accept event",
-          Date: "Mar 28",
-          Time: "14:09:55",
-          Host: "ip-10-77-20-248",
-          PID: "29069",
-          Message: "Accepted publickey",
-          User: "ubuntu",
-          Source_IP: "85.245.107.41",
-          Source_port: "55779",
-          Key: "Kl8kPGZrTiz7g4FO1hyqHdsSBBb5Fge6NWOobN03XJg"
+          Type: 'Accept event',
+          Date: 'Mar 28',
+          Time: '14:09:55',
+          Host: 'ip-10-77-20-248',
+          PID: '29069',
+          Message: 'Accepted publickey',
+          User: 'ubuntu',
+          Source_IP: '85.245.107.41',
+          Source_port: '55779',
+          Key: 'Kl8kPGZrTiz7g4FO1hyqHdsSBBb5Fge6NWOobN03XJg'
         }
       expect(result).to eq(expected)
     end
   end
 
-  invalid_user = "Mar 31 06:34:36 ip-10-77-20-248 sshd[18539]: Invalid user pruebas from 60.187.118.40" # 3741
-  describe "parse_invalid_user" do
+  invalid_user = 'Mar 31 06:34:36 ip-10-77-20-248 sshd[18539]: Invalid user pruebas from 60.187.118.40' # 3741
+  describe 'parse_invalid_user' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for an invalid user event" do
-      result = log_parser.parse_invalid_user(invalid_user, 3741, "Invalid user", "Mar 31", "06:34:36",
-                                             "ip-10-77-20-248")
+    it 'returns a hash of meta data for an invalid user event' do
+      result = log_parser.parse_invalid_user( invalid_user, 3741, 'Invalid user', 'Mar 31', '06:34:36',
+                                              'ip-10-77-20-248')
       expected =
         {
           Line_number: 3741,
-          Type: "Invalid user",
-          Date: "Mar 31",
-          Time: "06:34:36",
-          Host: "ip-10-77-20-248",
-          PID: "18539",
-          User: "pruebas",
-          Source_IP: "60.187.118.40",
+          Type: 'Invalid user',
+          Date: 'Mar 31',
+          Time: '06:34:36',
+          Host: 'ip-10-77-20-248',
+          PID: '18539',
+          User: 'pruebas',
+          Source_IP: '60.187.118.40'
         }
       expect(result).to eq(expected)
     end
   end
 
-  failed_password = "Mar 31 06:34:38 ip-10-77-20-248 sshd[18539]: Failed password for invalid user pruebas from 60.187.118.40 port 41838 ssh2" # 3745
-  describe "parse_failed_password" do
+  failed_password = 'Mar 31 06:34:38 ip-10-77-20-248 sshd[18539]: Failed password for invalid user pruebas from
+  60.187.118.40 port 41838 ssh2' # 3745
+  describe 'parse_failed_password' do
     let(:log_parser) { LogParser.new }
-    it "returns a hash of meta data for a failed password event" do
-      result = log_parser.parse_failed_password(failed_password, 3745, "Failed password", "Mar 31", "06:34:38",
-                                                "ip-10-77-20-248")
+    it 'returns a hash of meta data for a failed password event' do
+      result = log_parser.parse_failed_password(failed_password, 3745, 'Failed password', 'Mar 31', '06:34:38',
+                                                'ip-10-77-20-248')
       expected =
         {
           Line_number: 3745,
-          Type: "Failed password",
-          Date: "Mar 31",
-          Time: "06:34:38",
-          Host: "ip-10-77-20-248",
-          PID: "18539",
-          User: "pruebas",
-          Source_IP: "60.187.118.40",
-          Source_port: "41838"
+          Type: 'Failed password',
+          Date: 'Mar 31',
+          Time: '06:34:38',
+          Host: 'ip-10-77-20-248',
+          PID: '18539',
+          User: 'pruebas',
+          Source_IP: '60.187.118.40',
+          Source_port: '41838'
         }
       expect(result).to eq(expected)
     end
   end
 
-  describe "read_log" do
+  describe 'read_log' do
     let(:log_parser) { LogParser.new }
-    it "parses log into a structured format" do
-      log = log_parser.read_log("./data/auth.log")
+    it 'parses log into a structured format' do
+      log = log_parser.read_log('./data/auth.log')
       expect(log[:Error].length).to eq(189)
       expect(log[:Auth_failure].length).to eq(673)
       expect(log[:Disconnect].length).to eq(307)
@@ -206,20 +210,23 @@ describe "Preprocess log" do
     end
   end
 
-  describe "sanitize_date" do
+  describe 'sanitize_date' do
     let(:log_parser) { LogParser.new }
-    it "converts a string date into a date object" do
-      result = log_parser.sanitize_date("Apr 14")
-      expect(result).to eq("2025-04-14")
+    it 'converts a string date into a date object' do
+      result = log_parser.sanitize_date('Apr 14')
+      expect(result).to eq('2025-04-14')
     end
   end
 
-  describe "get_date_range" do
+  describe 'get_date_range' do
     let(:log_parser) { LogParser.new }
-    it "returns first and last date from log" do
-      result = log_parser.get_date_range()
-      expected = { "2025-03-27" => 0, "2025-03-28" => 0, "2025-03-29" => 0, "2025-03-30" => 0, "2025-03-31" => 0, "2025-04-01" => 0,
-                   "2025-04-02" => 0, "2025-04-03" => 0, "2025-04-04" => 0, "2025-04-05" => 0, "2025-04-06" => 0, "2025-04-07" => 0, "2025-04-08" => 0, "2025-04-09" => 0, "2025-04-10" => 0, "2025-04-11" => 0, "2025-04-12" => 0, "2025-04-13" => 0, "2025-04-14" => 0, "2025-04-15" => 0, "2025-04-16" => 0, "2025-04-17" => 0, "2025-04-18" => 0, "2025-04-19" => 0, "2025-04-20" => 0 }
+    it 'returns first and last date from log' do
+      result = log_parser.get_date_range
+      expected = {  '2025-03-27' => 0, '2025-03-28' => 0, '2025-03-29' => 0, '2025-03-30' => 0, '2025-03-31' => 0,
+                    '2025-04-01' => 0, '2025-04-02' => 0, '2025-04-03' => 0, '2025-04-04' => 0, '2025-04-05' => 0,
+                    '2025-04-06' => 0, '2025-04-07' => 0, '2025-04-08' => 0, '2025-04-09' => 0, '2025-04-10' => 0,
+                    '2025-04-11' => 0, '2025-04-12' => 0, '2025-04-13' => 0, '2025-04-14' => 0, '2025-04-15' => 0,
+                    '2025-04-16' => 0, '2025-04-17' => 0, '2025-04-18' => 0, '2025-04-19' => 0, '2025-04-20' => 0 }
       expect(result).to eq(expected)
     end
   end
