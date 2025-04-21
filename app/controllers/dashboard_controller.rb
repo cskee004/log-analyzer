d07b175f40fdf836cea27f9409c63696f1fe33ae
Chris Skeens <cskee004@odu.edu>
2025-04-21 15:36:41 -0400

    SMACSS

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,4 @@
 class DashboardController < ApplicationController
-  def index
+  def dashboard
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
+  root "dashboard#dashboard"
+  get "upload", to: "dashboard/upload"
+  get "summary", to: "dashboard/summary"
+  get "graph", to: "dashboard/graph"
+  get "table", to: "dashboard/table"
 
-  root "dashboard#index"
-  get "dashboard/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
